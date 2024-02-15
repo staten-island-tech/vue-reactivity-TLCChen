012e54bac47e325b934040bace55ac73c60a3aaf
TLCChen <139132998+TLCChen@users.noreply.github.com>
2024-02-14 23:18:07 -0500

    2-14-24

--- vueproject1/src/main.js
+++ vueproject1/src/main.js
@@ -1,14 +1,125 @@
-import './assets/main.css'
+import "./assets/main.css";
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
 
-import App from './App.vue'
-import router from './router'
+import App from "./App.vue";
+import router from "./router";
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(createPinia())
-app.use(router)
+app.use(createPinia());
+app.use(router);
 
-app.mount('#app')
+app.mount("#app");
+
+document.body.insertAdjacentHTML(
+  "afterbegin",
+  `
+
+<div class="bigBox2"></div>
+<div class="bigBox3"></div>
+<div class="bigBox4"></div>
+<div class="bigBox5"></div>
+
+
+
+
+`
+);
+
+document.querySelector(".bigBox").insertAdjacentHTML(
+  //make this a DOMSselector object later.
+  "beforeend",
+  `<div class="box">
+    </div>
+    <div class="box">
+    </div>
+    <div class="box">
+    </div>
+    <div class="box">
+    </div>
+    <div class="box">
+    </div>
+    `
+);
+document.querySelector(".bigBox2").insertAdjacentHTML(
+  //make this a DOMSselector object later.
+  "beforeend",
+  `<div class="box">
+    </div>
+    <div class="box">
+    </div>
+    <div class="box">
+    </div>
+    <div class="box">
+    </div>
+    <div class="box">
+    </div>
+    `
+);
+document.querySelector(".bigBox3").insertAdjacentHTML(
+  //make this a DOMSselector object later.
+  "beforeend",
+  `<div class="box">
+    </div>
+    <div class="box">
+    </div>
+    <div class="box">
+    </div>
+    <div class="box">
+    </div>
+    <div class="box">
+    </div>
+    `
+);
+document.querySelector(".bigBox4").insertAdjacentHTML(
+  //make this a DOMSselector object later.
+  "beforeend",
+  `<div class="box">
+    </div>
+    <div class="box">
+    </div>
+    <div class="box">
+    </div>
+    <div class="box">
+    </div>
+    <div class="box">
+    </div>
+    `
+);
+document.querySelector(".bigBox5").insertAdjacentHTML(
+  //make this a DOMSselector object later.
+  "beforeend",
+  `<div class="box">
+    </div>
+    <div class="box">
+    </div>
+    <div class="box">
+    </div>
+    <div class="box">
+    </div>
+    <div class="box">
+    </div>
+    `
+);
+
+document.querySelectorAll(".box").forEach((create) =>
+  create.insertAdjacentHTML(
+    "afterbegin",
+    `
+<button class="btn" type="submit">Button</button>
+`
+  )
+);
+
+document.querySelectorAll(".btn").forEach((create) =>
+  create.addEventListener("click", function () {
+    this.parentElement.insertAdjacentHTML(
+      "afterbegin",
+      `
+        <img src="favicon.ico" alt="">
+    `
+    );
+  })
+);
